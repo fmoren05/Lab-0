b4a8d326d2b7036846a104f457dc88226dd44ce2
fmoren05 <156385950+fmoren05@users.noreply.github.com>
2024-01-25 16:14:02 -0800

    docs folder

---
+++ docs/namespaces_dup.js
@@ -0,0 +1,7 @@
+var namespaces_dup =
+[
+    [ "main", "namespacemain.html", [
+      [ "plot_example", "namespacemain.html#a0ad5f912137251547c4523cbcdfb9190", null ],
+      [ "tk_matplot", "namespacemain.html#a928937c2fc9bad8410f8567662df4c34", null ]
+    ] ]
+];
---
+++ docs/navtreeindex0.js
@@ -0,0 +1,11 @@
+var NAVTREEINDEX0 =
+{
+"index.html":[],
+"namespacemain.html":[0,0,0],
+"namespacemain.html#a0ad5f912137251547c4523cbcdfb9190":[0,0,0,0],
+"namespacemain.html#a928937c2fc9bad8410f8567662df4c34":[0,0,0,1],
+"namespacemembers.html":[0,1,0],
+"namespacemembers_func.html":[0,1,1],
+"namespaces.html":[0,0],
+"pages.html":[]
+};
